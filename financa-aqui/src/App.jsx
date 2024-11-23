f7a8af9fdf595c450e8dca8ecc63b67bf973853a
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2024-11-22 23:04:22 -0300

    feat: dashboard config init

--- financa-aqui/src/App.jsx
+++ financa-aqui/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 export const App = () => {
   return (
---
+++ financa-aqui/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './Dashboard.css'
+
+const Dashboard = () => {
+    return (
+      <Text>Paxta</Text>
+    )
+  }
+  
+  export default Dashboard
